7b0d2276123c0e396bb863343662349477cdff2b
Cory <coryierso@gmail.com>
2022-07-23 11:59:31 -0400

    --wip-- [skip ci]

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=optional"
-            rel="stylesheet"
-          />
-        </Head>
-        <body className="bg-gray-50">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=optional"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="bg-gray-50">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
-export default MyDocument
