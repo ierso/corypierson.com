42604df570e4f18f105fd586f34f90388ee8a3f4
Cory <coryierso@gmail.com>
2022-08-14 21:21:12 -0400

    Update font weight.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=optional"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=optional"
           rel="stylesheet"
         />
       </Head>
